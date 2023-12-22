b3e30ab7a240b129fe090c58463b3d26a1412bf2
j-jiaxu <154569589+j-jiaxu@users.noreply.github.com>
2023-12-22 14:46:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ from pyecharts import options as opts
 from pyecharts.charts import WordCloud,Funnel
 import streamlit_echarts as ste
 import math
+import pandas as pd
 
+
+
+##############################################################################################################
 
 
 # 获取html文本
@@ -68,8 +72,27 @@ def a_tags_csv(word_counts):
 # 获取词频最高的前20个词
 def a_tags_top(word_counts):
     word_count = dict(Counter(word_counts).most_common(20))
-
     return word_count
+##############################################################################################################
+def common():
+    st.title('Web Scraping and Visualization App')
+    # 输入URL
+    url = st.text_input('Enter the URL:')
+
+    if url:
+        # 获取html文本
+        html=get_html(url)
+        # 获取数据
+        a_tags=get_data(html)
+        # 生成txt文本
+        a_tags_txt=get_txt(a_tags.text)
+        # 将词放入csv文件
+        word_counts=a_tags_read(a_tags_txt)
+        return word_counts
+    else:
+        return False
+#############################################绘制图形#################################################################
+
         
 # 绘制折线图
 def plot_line_chart(word_count):
@@ -103,26 +126,6 @@ def plot_bar_chart(word_count):
     plt.bar(word_count.keys(),word_count.values(),0.8,color='green')
     st.pyplot(fig)
 
-
-# 词云
-def plot_ciyun_chart(word_count):
-    plt.title('词云图')
-    # 将字典转为列表的形式
-    ls=list(word_count.items())
-    # 创建一个WordCloud类实例
-    wordcloud = (  
-        WordCloud()  
-    .add("", ls, word_size_range=[30, 30+len(word_count)],shape='circle')  
-    .set_global_opts(title_opts=opts.TitleOpts(title="词云图")))
-
-    # 保存为html文件
-    wordcloud.render("wordcloud.html")
-
-     # 读取 HTML 文件内容
-    with open('wordcloud.html', 'r', encoding='utf-8') as file:
-        html_content = file.read()
-    # 在页面中显示 HTML 内容
-    st.components.v1.html(html_content, height=500,width=900)
     
 # 散点图
 def plot_scatter_chart(word_count):
@@ -136,7 +139,6 @@ def plot_scatter_chart(word_count):
 
 # 直方图
 def plot_plotly_chart(word_count):
-    
     if st.button("Re-run"):
         fig=plt.figure(figsize=(10, 6))
         plt.title('直方图')
@@ -145,9 +147,10 @@ def plot_plotly_chart(word_count):
         plt.hist(word_count.values(),bins=len(word_count),color='yellow')
         st.pyplot(fig)
 
-def zz(word_count):
+# 雷达图
+def plot_leida_chart(word_count):
     words=list(word_count.keys())
-    counts=list(word_count.keys())
+    counts=list(word_count.values())
     n = len(counts)
     angles = [i * 2 * math.pi / n for i in range(n)]
     angles.append(angles[0])
@@ -171,56 +174,96 @@ def plot_ld_charts(word_count):
     wf.add('漏斗图',[list(z) for z in zip(word_count.keys(), word_count.values())])
     ste.st_pyecharts(wf)
 
-def pa_sidebar(selected_option,word_count):
+###############################################绘制词云###############################################################
+# 词云
+def plot_ciyun_chart(word_count,shape_mask):
+    plt.title('词云图')
+    # 将字典转为列表的形式
+    ls=list(word_count.items())
+    # 创建一个WordCloud类实例
+    wordcloud = (  
+        WordCloud()  
+    .add("", ls, word_size_range=[30, 30+len(word_count)],shape=shape_mask)  
+    .set_global_opts(title_opts=opts.TitleOpts(title="词云图")))
+
+    # 保存为html文件
+    wordcloud.render("wordcloud.html")
+
+     # 读取 HTML 文件内容
+    with open('wordcloud.html', 'r', encoding='utf-8') as file:
+        html_content = file.read()
+    # 在页面中显示 HTML 内容
+    st.components.v1.html(html_content, height=500,width=900)
+
+##############################################################################################################
+
+def get_word():
+    word_counts=common()
+    if word_counts:
+        # 上传 CSV 文件
+        uploaded_file = st.file_uploader("words1.csv", type=['csv'])
+
+        if uploaded_file is not None:
+            # 读取上传的 CSV 文件
+            data = pd.read_csv(uploaded_file)
+            # 将 CSV 文件内容以表格形式显示在页面上
+            st.cache_data(data)
+
+def Visualization():
+    #侧边栏选项
+    list_baidu_project= ['折线图', '饼图', '柱形图','直方图','散点图','雷达图','漏斗图']
+    selected_option = st.sidebar.selectbox("请选择图形类型",list_baidu_project)
+
+    word_counts=common()
+    # 获得词频最高的20个词
+    word_count=a_tags_top(word_counts)
+    # st.write(word_counts)
+
+    if word_count:
+        # 根据侧边栏选择显示不同的内容
+        if selected_option == "折线图":
+            plot_line_chart(word_count)
+        elif selected_option == "饼图":
+            plot_pie_chart(word_count)
+        elif selected_option == "柱形图":
+            plot_bar_chart(word_count)
+        elif selected_option == "直方图":
+            plot_plotly_chart(word_count)
+        elif selected_option == "散点图":
+            plot_scatter_chart(word_count)
+        elif selected_option == "漏斗图":
+            plot_ld_charts(word_count)
+        elif selected_option == "雷达图":
+            plot_leida_chart(word_count)
+
+
     
-    # 根据侧边栏选择显示不同的内容
-    if selected_option == "折线图":
-        plot_line_chart(word_count)
-    elif selected_option == "饼图":
-        plot_pie_chart(word_count)
-    elif selected_option == "柱形图":
-        plot_bar_chart(word_count)
-    elif selected_option == "词云图":
-        plot_ciyun_chart(word_count)
-    elif selected_option == "散点图":
-        plot_scatter_chart(word_count)
-    elif selected_option == "漏斗图":
-        plot_ld_charts(word_count)
-    elif selected_option == "雷达图":
-        zz(word_count)
+    
+# 词云
+def ciyun():
+    word_counts=common()
+    # 获得词频最高的20个词
+    word_count=a_tags_top(word_counts)
+    # 读取多个形状图片
+    shape_images = {'circle','rect','roundRect','triangle','diamond','pinwheel','snowflake','heart','star'}
+    shape_mask = st.sidebar.selectbox("shape",shape_images)
+
+    if word_count!=None:
+        plot_ciyun_chart(word_count,shape_mask)
+
+##############################################################################################################
 
 
 def main():
-    st.title('Web Scraping and Visualization App')
-    # 输入URL
-    url = st.text_input('Enter the URL:')
-    
-    #侧边栏标题
-    st.sidebar.title("实训")
-    #侧边栏选项
-    list_baidu_project= ['折线图', '饼图', '柱形图','词云图','散点图','雷达图','漏斗图']
-    selected_option = st.sidebar.selectbox("请选择图形类型",list_baidu_project)
+    # 创建侧边栏选项来切换页面
+    page = st.sidebar.radio("选择页面", ("word_count", "Visualization","ciyun"))
 
-
-    if url:
-        # 获取html文本
-        html=get_html(url)
-        # 获取数据
-        a_tags=get_data(html)
-        # 生成txt文本
-        a_tags_txt=get_txt(a_tags.text)
-        # 将词放入csv文件
-        word_counts=a_tags_read(a_tags_txt)
-        # 获得词频最高的20个词
-        word_count=a_tags_top(word_counts)
-        # 可视化
-        pa_sidebar(selected_option,word_count)
-
-
-    
-    
-    
-    
+    if page=="word_count":
+        get_word()
+    elif page=="Visualization":
+        Visualization()
+    elif page=="ciyun":
+        ciyun() 
 
 if __name__=='__main__':
     main()
